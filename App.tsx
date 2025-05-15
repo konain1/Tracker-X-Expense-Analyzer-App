a81501b44331dac96b4afcb181a1574a2dff7917
Konain1 <kaunainkasab@gmail.com>
2025-05-15 18:38:20 +0530

    added tailwind and typescript

--- App.tsx
+++ App.tsx
@@ -1,24 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import LoginScreen from './Screens/LoginScreen';
+import "./global.css"
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>TrackerX</Text>
+    <View className='flex-1  justify-center items-center' >
+      <Text className=' text-4xl font-bold' >TrackerX</Text>
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    fontSize:38,
-    fontWeight:'bold'
-  }
-});
---
+++ Screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+
+function LoginScreen(){
+    return(
+        <View>
+            <Text>Login</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
