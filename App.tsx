2d08aa9437d9cf3a6699dedc1aacf45b22dc2fe5
Konain1 <kaunainkasab@gmail.com>
2025-05-15 15:13:57 +0530

    trackerX initialized

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>TrackerX</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:38,
+    fontWeight:'bold'
+  }
 });
