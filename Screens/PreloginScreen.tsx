049cd2f775eb9322405708e10a597ba27c52512c
Konain1 <kaunainkasab@gmail.com>
2025-05-18 13:43:33 +0530

    pacman circle completed

--- Screens/PreloginScreen.tsx
+++ Screens/PreloginScreen.tsx
@@ -10,9 +10,23 @@ function PreloginScreen() {
           <Text className="text-emerald-700 text-6xl">Right Now</Text>
         </View>
       </View>
-      <View className="bg-black text-white h-[40%] rounded-b-[50%]">
+      <View className="bg-black text-white h-[40%] rounded-b-[50%] ">
         <View>
-          <View></View>
+          <View className=" w-40 h-40 rounded-[50%] bg-white items-end justify-end relative  top-[80%] left-[20%] ">
+            <View
+              className="h-0 w-0 rotate-90 absolute top-[45%] left-[45%]"
+              style={{
+                borderLeftWidth: 50,
+                borderRightWidth: 50,
+                borderBottomWidth: 100,
+                borderLeftColor: "transparent",
+                borderRightColor: "transparent",
+                borderBottomColor: "black",
+                borderStyle: "solid",
+              }}
+            ></View>
+            <View className="bg-yellow-200 w-[35%] h-[35%] rounded-full top-8"></View>
+          </View>
         </View>
       </View>
 
