d5a696a0e4e076db1b8471099f61a036049092ae
Ariz <khan.ariz2341997@gmail.com>
2025-05-18 14:17:51 +0530

    feat: added montserrat font support

--- App.tsx
+++ App.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { useFonts } from "expo-font";
 import LoginScreen from "./Screens/LoginScreen";
+import {
+  Montserrat_700Bold,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_300Light,
+  Montserrat_100Thin,
+} from "@expo-google-fonts/montserrat";
 import "./global.css";
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_100Thin,
+    Montserrat_600SemiBold,
+    Montserrat_300Light,
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) return <StatusBar style="auto" />; // or a splash screen
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className=" text-4xl font-bold">TrackerX</Text>
+      <Text className="font-bold text-4xl">TrackerX</Text>
       <LoginScreen />
-      <StatusBar style="auto" />
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        thin: ["Montserrat_100Thin"],
+        light: ["Montserrat_300Light"],
+        regular: ["Montserrat_400Regular"],
+        medium: ["Montserrat_500Medium"],
+        semiBold: ["Montserrat_600SemiBold"],
+        bold: ["Montserrat_700Bold"],
+      },
+    },
   },
   plugins: [],
 };
